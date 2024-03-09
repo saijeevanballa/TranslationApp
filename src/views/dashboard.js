d849b2aae8f65c2afb8e6a6d44fec45209541f68
Sai Jeevan Balla <saijeevanballa@gmail.com>
2024-03-09 14:42:01 +0530

    fix dashboard

--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -14,107 +14,88 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
-
-import Login from "./login"
-import { defaultTostStyle } from "../utils/constants"
-import { setToken } from "../store/actions/auth"
-import BasicModal from "../components/model/model";
-import TextArea from "../components/textArea/textArea";
+import Login from './login';
+import { defaultTostStyle } from '../utils/constants';
+import { setToken } from '../store/actions/auth';
+import BasicModal from '../components/model/model';
+import TextArea from '../components/textArea/textArea';
 import { fetchTranslateAPI } from '../store/actions/api';
 import Result from './result';
 import Languages from "../store/data/languages.json"
 
 function Dashboard(props) {
-    const [inputValue, setInputValue] = React.useState('plain');
+          const [inputValue, setInputValue] = React.useState('plain');
     const [textInput, setTextInput] = React.useState('');
-    const [tokenExist, setTokenExist] = React.useState(false);
-    const [resultExist, setResultExist] = React.useState({ open: false, value: "" });
+          const [tokenExist, setTokenExist] = React.useState(false);
+    const [resultExist, setResultExist] = React.useState({ open: false, 
+      
+      value: "" });
     const [selectedLanguage, setSelectedLanguage] = React.useState("");
 
+  const handleSubmit = async () => {
+    if (!props.token || props.token.trim() === '') {
 
-    const handleSubmit = async () => {
-        if (!props.token || props.token.trim() === "") {
-            return setTokenExist(true)
-        }
-        if (!selectedLanguage || !textInput) {
-            return toast("Please fill all the details", {
-                type: "error",
-                ...defaultTostStyle
-            });
-        }
-        let response = await props.fetchTranslateAPI({
-            data: textInput,
-            type: inputValue,
-            lan: selectedLanguage
-        })
-        if (response?.data) {
-            setResultExist({ open: true, value: response.data.data.translated })
-            toast("Successfully Translation Done", {
-                type: "success",
-                ...defaultTostStyle
-            });
-        }
+      setTokenExist(true)
+      return ;
+    }
+    if (!selectedLanguage || !textInput) {
+      toast('Please fill all the details', { type: 'error', ...defaultTostStyle });
+      return ;
+    }
+    let response = await props.fetchTranslateAPI({ 
+      data: textInput, type: inputValue, 
+      
+      lan: selectedLanguage });
+    if (response?.data) {
+      setResultExist({ open: true, value: response.data.data.translated });
+      
+      toast('Successfully Translation Done', { type: 'success', ...defaultTostStyle });
     }
 
-    let handleLoginSubmit = async (token) => {
-        if (!token || token.length !== 50) {
-            return toast("Enter valid Token", {
-                type: "error",
-                ...defaultTostStyle
-            });
-        }
-        props.setToken(token)
-        setTokenExist(false)
+  const handleLoginSubmit = async (token) => {
+    const newToken = token
+    if (!newToken || newToken.length !== 50) {
+      return toast('Enter valid Token', { type: 'error', ...defaultTostStyle });
     }
+    props.setToken(newToken);
+    setTokenExist(false);
+  };
 
-    let handleCloseResult = async () => {
-        setResultExist({ ...resultExist, open: false })
-    }
+  const handleCloseResult = async () => {
+    let open = false
+    setResultExist({ ...resultExist, open: open });
+  };
 
-    return (
-        <div style={{ marginTop: "20px" }}>
-            <Box sx={{ minWidth: 275 }}>
-                <Card variant="outlined" style={props.style} >{
-                    <React.Fragment>
-                        <BasicModal open={resultExist.open}><Result value={resultExist.value} handleSubmit={handleCloseResult} /></BasicModal>
-                        <BasicModal open={tokenExist}><Login handleSubmit={handleLoginSubmit} /></BasicModal>
-                        <CardContent sx={{ minWidth: "60%" }}>
-
-                            <TextArea textInput={textInput} setTextInput={setTextInput} />
-                            <FormControl style={{ margin: "5px" }}>
-                                <RadioGroup
-                                    row
-                                    aria-labelledby="demo-row-radio-buttons-group-label"
-                                    name="row-radio-buttons-group"
-                                    value={inputValue}
-                                    onChange={(e) => setInputValue(e.target.value)}
-                                >
-                                    {props.inputs.map(input => <FormControlLabel key={input} value={input} control={<Radio />} label={input.toUpperCase()} />)}
-                                </RadioGroup>
-                            </FormControl>
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-simple-select-label">Language</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    value={selectedLanguage}
-                                    label="Language"
-                                    style={{ borderRadius: "15px" }}
-                                    onChange={e => setSelectedLanguage(e.target.value)}
-                                >
-                                    {Languages.map(lan => (<MenuItem key={lan.code} value={lan.code}>{lan.language}</MenuItem>))}
-                                </Select>
-                            </FormControl>
-
-                        </CardContent>
-                        <CardActions>
-                            <Button variant="outlined" size="medium" onClick={handleSubmit}>Submit</Button>
-                        </CardActions>
-                    </React.Fragment>
-                }</Card>
-            </Box>
-        </div>
-    )
+  return (
+    <div style={{ marginTop: '20px' }}>
+      <Box sx={{ minWidth: 275 }}>
+        <Card variant="outlined" style={props.style}>
+          {
+            <React.Fragment>
+              <BasicModal open={resultExist.open}><Result value={resultExist.value} handleSubmit={handleCloseResult} /></BasicModal>
+              <BasicModal open={tokenExist}><Login handleSubmit={handleLoginSubmit} /></BasicModal>
+              <CardContent sx={{ minWidth: '60%' }}>
+                <TextArea textInput={textInput} setTextInput={setTextInput} />
+                <FormControl style={{ margin: '5px' }}>
+                  <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group" value={inputValue} onChange={(e) => setInputValue(e.target.value)}>
+                    {props.inputs.map(input => <FormControlLabel key={input} value={input} control={<Radio />} label={input.toUpperCase()} />)}
+                  </RadioGroup>
+                </FormControl>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Language</InputLabel>
+                  <Select labelId="demo-simple-select-label" id="demo-simple-select" value={selectedLanguage} label="Language" style={{ borderRadius: '15px' }} onChange={e => setSelectedLanguage(e.target.value)}>
+                    {Languages.map(lan => (<MenuItem key={lan.code} value={lan.code}>{lan.language}</MenuItem>))}
+                  </Select>
+                </FormControl>
+              </CardContent>
+              <CardActions>
+                <Button variant="outlined" size="medium" onClick={handleSubmit}>Submit</Button>
+              </CardActions>
+            </React.Fragment>
+          }</Card>
+      </Box>
+    </div>
+  );
 }
 
 Dashboard.propTypes = {
@@ -123,29 +104,15 @@ Dashboard.propTypes = {
 }
 
 Dashboard.defaultProps = {
-    inputs: ["plain", "json", "html", "android", "ios", "srt", "xamarin", "yaml"],
-    style: {
-        display: "flex",
-        alignItems: "center",
-        flexDirection: "column",
-        borderRadius: "15px",
-        boxShadow: `-15px -15px 15px rgba(255,255,255,0.2),
-                    15px 15px 15px rgba(0,0,0,0.1),
-                    inset -15px -15px 15px rgba(255,255,255,0.2),
-                    inset 15px 15px 15px rgba(0,0,0,0.1)
-                    `,
-        padding: "30px"
-    }
+  inputs: ['plain', 'json', 'html', 'android', 'ios', 'srt', 'xamarin', 'yaml'],
+  style: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    borderRadius: '15px',
+    boxShadow: `-15px -15px 15px rgba(255,255,255,0.2), 15px 15px 15px rgba(0,0,0,0.1), inset -15px -15px 15px rgba(255,255,255,0.2), inset 15px 15px 15px rgba(0,0,0,0.1)`,
+    padding: '30px'
+  }
+};
 
-}
-
-export default connect(
-    state => {
-        return { token: state.authentication.token };
-    }, {
-    setToken,
-    fetchTranslateAPI
-}
-)(Dashboard);
-
-
+export default connect(state => ({ token: state.authentication.token }), { setToken, fetchTranslateAPI })(Dashboard);
